b9a4d7d0f85d1c6c446a0669e9bd2f5534aa9934
abbubai-dev <drg.abbubai@gmail.com>
2025-10-23 01:48:45 +0800

    updates API

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
-// API configuration - Google Apps Script Web App URL (provided)
-export const API_URL = "https://script.google.com/macros/s/AKfycbxPCh3ANaZAl_kc2StbF19scMAyKDzQZv2n746FvVGHTJk3urIltB3qn59HNEUY4_ZQ/exec";
+// Proxy endpoint (Netlify)
+export const API_URL = "/api"; 
+
 // Helper endpoints
 export const GET_ITEMS = API_URL + "?action=getItems";
 export const GET_TRANSACTIONS = API_URL + "?action=getTransactions";
